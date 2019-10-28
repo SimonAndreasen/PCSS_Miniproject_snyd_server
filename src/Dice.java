f8f5f0c4fb8d8853be4bd21cb893b27a65ee3016
Connie RageCage <43566860+Alex696969@users.noreply.github.com>
2019-10-28 16:23:26 +0100

    made everything work and added dice, shuffle, printdice to Users.

---
+++ src/Dice.java
@@ -0,0 +1,13 @@
+
+
+public class Dice {
+
+    public int value;
+
+    public Dice(){
+        value = 0;
+    }
+    public void roll(){
+        value = (int) (Math.random() * ((6 - 1) + 1)) + 1;
+    }
+}
--- src/Users.java
+++ src/Users.java
@@ -2,6 +2,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Users extends Thread {
     private Server server;
@@ -11,11 +12,29 @@ public class Users extends Thread {
     private DataInputStream input;
     //number of clients
     private int clientNo = 0;
+    ArrayList<Dice> myDice = new ArrayList<>();
 
     //text area for displaying context
     public Users(Server server, Socket socket) {
         this.server = server;
         this.socket = socket;
+        myDice.add(new Dice());
+        myDice.add(new Dice());
+        myDice.add(new Dice());
+        myDice.add(new Dice());
+    }
+
+        public void shuffle(){
+            for (int i=0; i<myDice.size(); i++){
+                myDice.get(i).roll();
+
+            }
+
+    }
+    public void printDice(){
+        System.out.println("Your dice: ");
+        myDice.forEach( (i) -> System.out.print(i.value + ", "));
+        System.out.println(" ");
     }
 
     @Override
