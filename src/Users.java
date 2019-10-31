5ac3a7cb3d08a229e1d704ad621f6ab81dff12f4
Connie RageCage <43566860+Alex696969@users.noreply.github.com>
2019-10-31 12:30:50 +0100

    moved all dice related stuff to new fancy dice class

--- src/Users.java
+++ src/Users.java
@@ -30,30 +30,10 @@ public class Users extends Thread {
     public void shuffle() {
         for (int i = 0; i < myDice.size(); i++) {
             myDice.get(i).roll();
-
         }
 
     }
 
-    public class Dice {
-
-        public int value;
-
-        public Dice(){
-            value = 0;
-        }
-        public void roll(){
-            value = (int) (Math.random() * ((6 - 1) + 1)) + 1;
-        }
-
-    }
-
-
-    public void printDice() {
-        System.out.println("Your dice: ");
-        myDice.forEach((i) -> System.out.print(i.value + ", "));
-        System.out.println(" ");
-    }
 
     @Override
     public void run() {
