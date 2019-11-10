708bbde9697e5c9374bf794563d52e17c95400fc
Alex Ventegodt <sban18@student.aau.dk>
2019-11-10 21:26:01 +0100

    Deleted unused code in duce

--- src/Dice.java
+++ src/Dice.java
@@ -5,24 +5,3 @@ public class Dice {
     public Dice() {
         value = (int) (Math.random() * ((6 - 1) + 1)) + 1;}
 }
-
-   /* public void printDice() {
-        System.out.println("Your dice: ");
-        myDice.forEach((i) -> System.out.print(i.value + ", "));
-      System.out.println(" ");
-   }
-
-    public void createDice(){
-        //Create three instance of dice class
-        for (int i = 0; i < 4; i++) {
-          //  myDice.add(new Dice());
-        }
-    }
-
-    public void shuffle() {
-        //for (int i = 0; i < myDice.size(); i++) {
-       //     myDice.get(i).roll();
-        }
-
-    }
-*/
